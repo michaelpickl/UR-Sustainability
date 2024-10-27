8051a2b6c6ce533b4c13be12419a9c45fe5e4063
baa43136 <anna-lena.babl@stud.uni-regensburg.de>
2024-10-27 19:17:12 +0100

    Added second page to start menu

--- Assets/Scripts/Start/StartMenu.cs
+++ Assets/Scripts/Start/StartMenu.cs
@@ -1,8 +1,45 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class StartMenu : MonoBehaviour
 {
+    public TMP_Text textHeading;
+    public TMP_Text textIntro;
+    public TMP_Text textControl;
+
+    public Button buttonNext;
+    public Button buttonBack;
+    public Button buttonStart;
+
+    private void Start()
+    {
+        ShowIntro(); // Startet mit dem Intro-Text
+    }
+
+    public void ShowIntro()
+    {
+        textHeading.gameObject.SetActive(true);
+        textIntro.gameObject.SetActive(true);
+        buttonNext.gameObject.SetActive(true);
+
+        textControl.gameObject.SetActive(false);
+        buttonBack.gameObject.SetActive(false);
+        buttonStart.gameObject.SetActive(false);
+    }
+
+    public void ShowControls()
+    {
+        textHeading.gameObject.SetActive(false);
+        textIntro.gameObject.SetActive(false);
+        textControl.gameObject.SetActive(true);
+        buttonBack.gameObject.SetActive(true);
+        buttonStart.gameObject.SetActive(true);
+
+        buttonNext.gameObject.SetActive(false);
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("Gebäude");
