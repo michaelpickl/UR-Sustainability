bf5094b8af4ba3ecf5f19060327c7f986169b41b
EmmaSophieReichert <emmasophie1203@gmx.de>
2024-10-31 17:40:46 +0100

    Add Credits Page

--- Assets/Scripts/End/EndMenu.cs
+++ Assets/Scripts/End/EndMenu.cs
@@ -6,11 +6,41 @@ using TMPro;
 public class EndMenu : MonoBehaviour
 {
     public TMP_Text textMeasuresAndCO2;
+    public Button buttonCredits;
+
+    public Button buttonRestart;
+    public Button buttonBack;
+
+    public GameObject endPanel;
+    public GameObject creditsPanel;
 
     private void Start()
     {
         string textMeasures = "<b>" + PlayerPrefs.GetInt("MeasuresDone") + "/" + PlayerPrefs.GetInt("MeasuresAll") + "</b>";
         string textC02 = "<b>" + PlayerPrefs.GetInt("CO2%Saved") + "%</b>";
         textMeasuresAndCO2.text = "Immerhin konntest du " + textMeasures + " der Maßnahmen umsetzen und damit " + textC02 + " des jährlichen CO2-Ausstoßes der Universität Regensburg einsparen.";
+    }
+
+    public void ShowCredits()
+    {
+        print("CREDITS");
+        endPanel.SetActive(false);
+        creditsPanel.SetActive(true);
+        buttonCredits.gameObject.SetActive(false);
+        buttonBack.gameObject.SetActive(true);
+    }
+
+    public void ShowEnd()
+    {
+        endPanel.SetActive(true);
+        creditsPanel.SetActive(false);
+        buttonCredits.gameObject.SetActive(true);
+        buttonBack.gameObject.SetActive(false);
+    }
+
+    public void RestartGame()
+    {
+        print("Restart");
+        SceneManager.LoadScene("Start");
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # UR-Sustainability
 In this project, gamification is used to show students and employees of the University of Regensburg possible measures that are needed to make the university carbon-free by 2050.
+
+# Credits
+<b>Kartenmaterial:</b> Open Street Map (https://www.openstreetmap.org/)
+<b>Blosm-Plugin für Blender:</b> https://blender-addons.org/blosm/
+<b>RTS-Kamera:</b> https://assetstore.unity.com/packages/tools/camera/rts-camera-43321
+<b>SimplePoly City - Low Poly Assets: </b> https://assetstore.unity.com/packages/3d/environments/simplepoly-city-low-poly-assets-58899
+<b>Toon City Pack: </b> https://assetstore.unity.com/packages/3d/environments/urban/toon-city-pack-234785
